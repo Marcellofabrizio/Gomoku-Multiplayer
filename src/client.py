a15aeeab62537c2e18cba4cb0b2ab7a9cc47b534
Marcello Fabrizio <marcello@pop-os.localdomain>
2022-09-17 18:29:59 -0300

    First

---
+++ src/client.py
@@ -0,0 +1,28 @@
+import socket
+from _thread import *
+
+host = '127.0.0.1'
+port = 1234
+
+def response_handler(connection):
+    while True:
+        response = connection.recv(4096)
+        print(response.decode('utf-8'))
+
+clientSocket = socket.socket()
+
+print('Atempting connecting to host...')
+
+try:
+    clientSocket.connect((host, port))
+except socket.error as err:
+    print(str(err))
+
+response = clientSocket.recv(4096)
+
+start_new_thread(response_handler, (clientSocket,))
+while True:
+    msg = input()
+    clientSocket.send(str.encode(msg))
+
+clientSocket.close()
---
+++ src/server.py
@@ -0,0 +1,51 @@
+import socket
+from _thread import *
+
+host = '127.0.0.1'
+port = 1234
+
+client_list = []
+
+def connection_handler(connection, player):
+    connection.send(str.encode("Hello client"))
+    
+    while True:
+        data = connection.recv(4096)
+        message = data.decode('utf-8')
+        if message == 'BYE':
+            break
+        
+        if player == 0:
+            client_list[1].sendall(str.encode('Player 0 says: ' + message))
+
+        if player == 1:
+            client_list[0].sendall(str.encode('Player 1 says: ' + message))
+
+        reply = f'Server: {message}'
+        print('Player: ' + str(player))
+        connection.sendall(str.encode(reply))
+
+    connection.close()
+
+def accept_connection(socket, current_player):
+    client, addr = socket.accept()
+    print(f'Connected to: {addr[0]}:{addr[1]}')
+    client_list.append(client)
+    start_new_thread(connection_handler, (client, current_player))
+
+def start_server(host, port):
+    current_player = 0
+    serverSocket = socket.socket()
+    try:
+        serverSocket.bind((host, port))
+    except socket.error as err:
+        print(str(err))
+
+    print('Server listening at port ' + str(port))
+    serverSocket.listen(2)
+
+    while True:
+        accept_connection(serverSocket, current_player)
+        current_player += 1
+
+start_server(host, port)
