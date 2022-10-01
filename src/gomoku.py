2089b7123371347ea2d4558452ca17e847de93b5
KevinPegor <kgpegoraro@ucs.br>
2022-10-01 20:18:05 -0300

    Verificação de vitória
    verifica se os jogadores ganharam ou não

--- src/gomoku.py
+++ src/gomoku.py
@@ -2,12 +2,50 @@ import pygame
 import itertools
 import pickle
 import numpy as np
+import re
 
 from _thread import *
 from queue import Queue
 import threading
 
 tasks = Queue()
+plays = [
+    [(0,-4), (0,-3), (0,-2), (0,-1), (0,0)],
+    [(0,-3), (0,-2), (0,-1), (0,0), (0,1)],
+    [(0,-2), (0,-1), (0,0), (0,1), (0,2)],
+    [(0,-1), (0,0), (0,1), (0,2), (0,3)],
+    [(0,0), (0,1), (0,2), (0,3), (0,4)],
+
+    [(0,0), (-1,0), (-2,0), (-3,0), (-4,0)],
+    [(1,0), (0,0), (-1,0), (-2,0), (-3,0)],
+    [(2,0), (1,0), (0,0), (-1,0), (-2,0)],
+    [(3,0), (2,0), (1,0), (0,0), (-1,0)],
+    [(4,0), (3,0), (2,0), (1,0), (0,0)],
+
+    [(-4,-4), (-3,-3), (-2,-2), (-1,-1), (0,0)],
+    [(-3,-3), (-2,-2), (-1,-1), (0,0), (1,1)],
+    [(-2,-2), (-1,-1), (0,0), (1,1), (2,2)],
+    [(-1,-1), (0,0), (1,1), (2,2), (3,3)],
+    [(0,0), (1,1), (2,2), (3,3), (4,4)],
+
+    [(4,4), (3,3), (2,2), (1,1), (0,0)],
+    [(3,3), (2,2), (1,1), (0,0), (-1,-1)],
+    [(2,2), (1,1), (0,0), (-1,-1), (-2,-2)],
+    [(1,1), (0,0), (-1,-1), (-2,-2), (-3,-3)],
+    [(0,0), (-1,-1), (-2,-2), (-3,-3), (-4,-4)],
+
+    [(0,0), (1,1), (2,2), (3,3), (4,4)],
+    [(-1,-1), (0,0), (1,1), (2,2), (3,3)],
+    [(-2,-2), (-1,-1), (0,0), (1,1), (2,2)],
+    [(-3,-3), (-2,-2), (-1,-1), (0,0), (3,3)],
+    [(-4,-4), (-3,-3), (-2,-2), (-1,-1), (0,0)],
+
+    [(0,0), (-1,-1), (-2,-2), (-3,-3), (-4,-4)],
+    [(1,1), (0,0), (-1,-1), (-2,-2), (-3,-3)],
+    [(2,2), (1,1), (0,0), (-1,-1), (-2,-2)],
+    [(3,3), (2,2), (1,1), (0,0), (-1,-1)],
+    [(4,4), (3,3), (2,2), (1,1), (0,0)]
+]
 
 class Colors:
     BLACK = (0, 0, 0)
@@ -62,9 +100,9 @@ class Gomoku:
         x_coord = (coord[0]*self.cols-1)//self.w
         y_coord = (coord[1]*self.rows-1)//self.h
 
-        for i in range(len(self.free_slots)):
+        '''for i in range(len(self.free_slots)):
             print(self.free_slots[i])
-        print()
+        print()'''
 
         if self.free_slots[x_coord, y_coord] != 0:
             return
@@ -73,6 +111,12 @@ class Gomoku:
         self.free_slots[x_coord, y_coord] = (self.turn%2)+1
         pygame.draw.circle(self.screen, color, coord, self.piece_size)
         pygame.display.update()
+
+        if self.win_play2(x_coord, y_coord):
+            print("WON 2.0")
+
+        #if self.win_play(x_coord, y_coord):
+            #print("won")
         
         if send_move:
             self.conn_i.send((x_coord, y_coord), self.turn)
@@ -89,6 +133,107 @@ class Gomoku:
     
     def serialize_board(self):
         return pickle.dumps(self.board)
+
+    def win_play(self,x,y):
+        turno = (self.turn%2)+1
+        for direction in plays:
+            try:
+                count = 0
+                for coord in direction:
+                    x_coord,y_coord = coord
+                    if self.free_slots[x+x_coord, y+y_coord] == turno:
+                        count += 1
+
+                if count == 5:
+                    return True
+            except IndexError:
+                continue
+        return False
+
+    def win_play2(self,x,y):
+        turno = (self.turn%2)+1
+
+        if self.diagonal_principal(x,y,turno):
+            print("funcionou principal")
+            return True
+        
+        if self.diagonal_secundaria(x,y,turno):
+            print("funcionou secundaria")
+            return True
+
+        if self.vertical(turno):
+            print("funcionou vertical")
+            return True
+
+        if self.horizontal(turno):
+            print("funcionou horizontal")
+            return True
+
+    def diagonal_principal(self, x, y, turno):
+        new_x = 0
+        new_y = 0
+        vetor = []
+
+        if x > y:
+            new_x = x - y
+        elif x < y:
+            new_y = y - x
+
+        while(new_x < 16 and new_y < 16):
+            vetor.append(int(self.free_slots[new_x, new_y]))
+            new_x += 1
+            new_y += 1
+
+        string_diagonal = "".join(map(str,vetor))
+
+        retorno = re.search(str(turno) + "{5}", string_diagonal)
+
+        return retorno != None
+
+    def diagonal_secundaria(self, x, y, turno):
+        new_x = 0
+        new_y = 0
+        vetor = []
+
+        if x > y:
+            new_y = x + y
+        elif x < y:
+            new_x = x + y
+
+        while(new_x >= 0 and new_y < 16):
+            vetor.append(int(self.free_slots[new_x, new_y]))
+            new_x -= 1
+            new_y += 1
+
+        string_diagonal = "".join(map(str,vetor))
+
+        retorno = re.search(str(turno) + "{5}", string_diagonal)
+
+        return retorno != None
+
+    def vertical(self, turno):
+        vetores = np.squeeze(np.asarray(self.free_slots))
+
+        string_vertical = "".join(map(str,vetores))
+
+        pattern = "(" + str(turno)+ ". " + ")" +  "{5}"
+
+        retorno = re.search(pattern, string_vertical)
+
+        return retorno != None
+
+    def horizontal(self, turno):
+        teste = np.transpose(self.free_slots)
+
+        vetores = np.squeeze(teste)
+
+        string_horizontal = "".join(map(str,vetores))
+
+        pattern = "(" + str(turno)+ ". " + ")" +  "{5}"
+
+        retorno = re.search(pattern, string_horizontal)
+
+        return retorno != None
 
     def play(self):
         self.draw_board()
